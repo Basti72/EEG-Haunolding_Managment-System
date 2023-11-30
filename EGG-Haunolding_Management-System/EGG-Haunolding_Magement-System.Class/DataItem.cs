0129c61ad2483f538d12cb889846b9705646323d
Bell Florian <Florian.BELL@htlvb.at>
2023-11-30 10:18:30 +0100

    Added DataItem

---
+++ EGG-Haunolding_Management-System/EGG-Haunolding_Magement-System.Class/DataItem.cs
@@ -0,0 +1,13 @@
+﻿namespace EGG_Haunolding_Magement_System.Class
+{
+    public class DataItem
+    {
+        public string Origin { get; set; }
+
+        public DateTime Time { get; set; }
+
+        public int Saldo { get; set; }
+
+        public int SaldoAvrg { get; set; }
+    }
+}
