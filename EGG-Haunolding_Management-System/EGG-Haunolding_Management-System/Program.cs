55010f1acca1ddeb68b699f0a52cce620c7081e9
Oberndorfer Sebastian <oberndorfer.sebastian@gmail.com>
2023-12-04 15:45:49 +0100

    Final version with CanvasJS but need to migrate to Chart.js because of commercial use

--- EGG-Haunolding_Management-System/EGG-Haunolding_Management-System/Program.cs
+++ EGG-Haunolding_Management-System/EGG-Haunolding_Management-System/Program.cs
@@ -9,13 +9,13 @@ builder.Services.AddTransient<IDataStore>(ctx =>
 {
     return new StaticDataStore(new List<DataItem>
     {
-        new DataItem("Oberndorfer", new DateTime(2023, 12, 12, 12, 0, 0), 50, 52),
+        new DataItem("Oberndorfer", new DateTime(2023, 12, 12, 12, 2, 6), 50, 52),
         new DataItem("Bell", new DateTime(2023, 12, 12, 12, 0, 0), -12, -8),
         new DataItem("Roider", new DateTime(2023, 12, 12, 12, 0, 0), 0, 3),
-        new DataItem("Oberndorfer", new DateTime(2023, 12, 12, 12, 0, 30), 49, 52),
+        new DataItem("Oberndorfer", new DateTime(2023, 12, 12, 12, 10, 30), 49, 52),
         new DataItem("Bell", new DateTime(2023, 12, 12, 12, 0, 30), -7, -8),
         new DataItem("Roider", new DateTime(2023, 12, 12, 12, 0, 30), 2, 3),
-        new DataItem("Oberndorfer", new DateTime(2023, 12, 12, 12, 1, 0), 54, 52),
+        new DataItem("Oberndorfer", new DateTime(2023, 12, 12, 12, 17, 0), 54, 52),
         new DataItem("Bell", new DateTime(2023, 12, 12, 12, 1, 0), -8, -8),
         new DataItem("Roider", new DateTime(2023, 12, 12, 12, 1, 0), 5, 3)
     });
