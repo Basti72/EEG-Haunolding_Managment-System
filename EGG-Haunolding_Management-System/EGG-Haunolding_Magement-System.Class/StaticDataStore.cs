f593ba33f56757f894f423616b4f7a9ec3d1d079
Bell Florian <Florian.BELL@htlvb.at>
2023-12-05 13:45:34 +0100

    Added new functions to IDataStore

--- EGG-Haunolding_Management-System/EGG-Haunolding_Magement-System.Class/StaticDataStore.cs
+++ EGG-Haunolding_Management-System/EGG-Haunolding_Magement-System.Class/StaticDataStore.cs
@@ -1,4 +1,6 @@
-﻿namespace EGG_Haunolding_Magement_System.Class
+﻿using System.Text;
+
+namespace EGG_Haunolding_Magement_System.Class
 {
     public class StaticDataStore : IDataStore
     {
@@ -36,5 +38,28 @@
             return data;
         }
 
+        public List<DataItem> GetAllData()
+        {
+            return _data;
+        }
+
+        public string[] GetOrigins()
+        {
+            List<string> origins = new List<string>();
+
+            for(int i = 0; i < _data.Count; i++)
+            {
+                bool found = false;
+                for(int j = 0; j < origins.Count; j++)
+                {
+                    if (_data[i].Origin == origins[j])
+                        found = true;
+                }
+                if(!found)
+                    origins.Add(_data[i].Origin);
+            }
+
+            return origins.ToArray();
+        }
     }
 }
