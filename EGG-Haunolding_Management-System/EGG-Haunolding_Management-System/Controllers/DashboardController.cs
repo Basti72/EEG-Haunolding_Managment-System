5bc201d2f743b7b1567f665d5eb2476040cfe6cc
Oberndorfer Sebastian <oberndorfer.sebastian@gmail.com>
2023-12-06 11:16:51 +0100

    New dashboard Chart.js Test

--- EGG-Haunolding_Management-System/EGG-Haunolding_Management-System/Controllers/DashboardController.cs
+++ EGG-Haunolding_Management-System/EGG-Haunolding_Management-System/Controllers/DashboardController.cs
@@ -15,20 +15,41 @@ namespace EGG_Haunolding_Management_System.Controllers
 
         public ActionResult Index()
         {
-            var data = m_DataStore.GetAllDataByOrigin("Oberndorfer");
+            var data1 = m_DataStore.GetAllDataByOrigin("Oberndorfer");
+            var data2 = m_DataStore.GetAllDataByOrigin("Bell");
+            var data3 = m_DataStore.GetAllDataByOrigin("Roider");
+            List<string> times = new List<string>();
+            List<List<int>> values = new List<List<int>>();
+            List<int> valueTemp = new List<int>();
+            foreach (var item in data1)
+            {
+                times.Add(item.Time.ToString("yyyy-MM-dd HH:mm:ss"));
+                valueTemp.Add(item.Saldo);
+            }
+            values.Add(valueTemp);
+            values.Clear();
 
-            List<DataPoint> timeAndValues = new List<DataPoint>();
-            foreach(var x in data)
-                timeAndValues.Add(new DataPoint { X = Util.ToUnixTimestamp(x.Time), Y = x.Saldo });
+            foreach (var item in data2)
+            {
+                valueTemp.Add(item.Saldo);
+            }
+            values.Add(valueTemp);
+            values.Clear();
+
+            foreach (var item in data3)
+            {
+                valueTemp.Add(item.Saldo);
+            }
+            values.Add(valueTemp);
+            values.Clear();
 
             var model = new DashboardViewModel
             {
-                TimeAndValues = timeAndValues,
-                Title = "Saldo",
-                Origin = data[0].Origin
+                Times = times,
+                Values = new List<int>{43, 50, 47}
             };
+
             return View(model);
-            
         }
     }
 
--- EGG-Haunolding_Management-System/EGG-Haunolding_Management-System/Models/DashboardViewModel.cs
+++ EGG-Haunolding_Management-System/EGG-Haunolding_Management-System/Models/DashboardViewModel.cs
@@ -2,15 +2,8 @@ namespace EGG_Haunolding_Management_System.Models
 {
     public class DashboardViewModel
     {
-        public string Title { get; set; }
-        public string Origin { get; set; }
-        public List<DataPoint> TimeAndValues { get; set; }
+        public List<string> Times { get; set; }
+        public List<int> Values { get; set; }
 
-    }
-
-    public class DataPoint
-    {
-        public long X { get; set; }
-        public int Y { get; set; }
     }
 }
