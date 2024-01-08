28d0d5c9e6e81c007b95d797e094e45844d24307
Oberndorfer Sebastian <oberndorfer.sebastian@gmail.com>
2024-01-08 15:45:54 +0100

    changed to use only one origin and added dropdown menu to select that origin

--- EGG-Haunolding_Management-System/EGG-Haunolding_Management-System/Controllers/DashboardController.cs
+++ EGG-Haunolding_Management-System/EGG-Haunolding_Management-System/Controllers/DashboardController.cs
@@ -16,34 +16,38 @@ namespace EGG_Haunolding_Management_System.Controllers
         public ActionResult Index()
         {
             // Get all data by origin
-            List<List<DataItem>> dataList = new List<List<DataItem>>();
             var origins = m_DataStore.GetOrigins();
-            foreach(var origin in origins)
-                dataList.Add(m_DataStore.GetAllDataByOrigin(origin));
+            DashboardViewModel model = GetData(origins[0]);
+            model.Origins = origins.ToList();
+            return View(model);
+        }
+
+        private DashboardViewModel GetData(string origin)
+        {
+            var dataList = m_DataStore.GetAllDataByOrigin(origin);
             // Assign data from DB to local variables
             var times = new List<string>();
-            var values = new List<List<int>>();
-            for(int i = 0; i < dataList.Count; i++)
+            var values = new List<int>();
+            for (int i = 0; i < dataList.Count; i++)
             {
-                List<int> ints = new List<int>();
-                for(int j = 0; j < dataList[i].Count; j++)
-                {
-                    // Only assign time once
-                    if (i == 0)
-                        times.Add(dataList[i][j].Time.ToString("yyyy-MM-dd HH:mm:ss"));
-                    ints.Add(dataList[i][j].Saldo);
-                }
-                values.Add(ints);
+                times.Add(dataList[i].Time.ToString("yyyy-MM-dd HH:mm:ss"));
+                values.Add(dataList[i].Saldo);
             }
 
             var model = new DashboardViewModel
             {
                 Times = times,
                 Values = values,
-                Origins = origins.ToList()
+                Origin = origin
             };
+            return model;
+        }
 
-            return View(model);
+        public IActionResult UpdateChart(string origin)
+        {
+            var data = GetData(origin);
+            data.Origins = m_DataStore.GetOrigins().ToList();
+            return Json(data);
         }
     }
 
