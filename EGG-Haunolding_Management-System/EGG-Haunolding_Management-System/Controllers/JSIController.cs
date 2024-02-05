3d958c80ff770639b3216b4f671f8afb5be1aecb
Oberndorfer Sebastian <oberndorfer.sebastian@gmail.com>
2024-02-05 15:51:04 +0100

    First working js interpreter with some basic syntax highlighting

---
+++ EGG-Haunolding_Management-System/EGG-Haunolding_Management-System/Controllers/JSIController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Jint;
+using Jint.Native;
+using Jint.Runtime.Interop;
+using EGG_Haunolding_Management_System.Models;
+using EGG_Haunolding_Management_System.Class;
+using Jint.Native.Object;
+
+namespace EGG_Haunolding_Management_System.Controllers
+{
+    public class JSIController : Controller
+    {
+        private readonly IDataStore m_DataStore;
+        public JSIController(IDataStore dataStore)
+        {
+            m_DataStore = dataStore;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ExecuteJavaScript(JSIViewModel model)
+        {
+            var engine = new Engine(cfg => cfg.AllowClr());
+            try
+            {
+                var result = engine.Execute(model.Code).GetCompletionValue().ToString();
+                ViewBag.Result = result;
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+            }
+
+            return View(nameof(Index));
+        }
+    }
+}
---
+++ EGG-Haunolding_Management-System/EGG-Haunolding_Management-System/Models/JSIViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace EGG_Haunolding_Management_System.Models
+{
+    public class JSIViewModel
+    {
+        public string Code { get; set; }
+    }
+}
